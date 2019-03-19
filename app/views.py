1d825fd140a8fdc4401f74966723a540fd437c48
RachelleT <rache_trotman@hotmail.com>
2019-03-18 20:50:13 -0500

    Commit

--- app/views.py
+++ app/views.py
@@ -62,45 +62,6 @@ def profiles():
 def userprofile(userid):
 	user = UserProfile.query.filter_by(id = userid).first()
 	return render_template('userprofile.html', user = user)
-	
-@app.route('/secure-page')
-@login_required
-def secure_page():
-    """Render the website's secure page."""
-    return render_template('secure_page.html')
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.username.data:
-            # Get the username and password values from the form.
-            username = form.username.data
-            password = form.password.data
-
-            # using your model, query database for a user based on the username           
-            # and password submitted. Remember you need to compare the password hash.
-            # You will need to import the appropriate function to do so.
-            # Then store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method below.
-            
-            user = UserProfile.query.filter_by(username=username).first()
-            
-            if user is not None and check_password_hash(user.password, password):
-
-            # get user id, load into session
-            	
-            	login_user(user)
-            	
-            	flash('Logged in successfully.')
-
-            # remember to flash a message to the user
-            	return redirect(url_for('secure_page'))  # they should be redirected to a secure-page route instead
-    return render_template("login.html", form=form)
-
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
